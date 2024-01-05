9da5b0718aa73740e0dbe9f1ee3647a7e49d6981
3xysha <3xysha@gmail.com>
2024-01-05 15:34:51 +0530

    version

--- pub.go
+++ pub.go
@@ -1,20 +1,9 @@
 package pub
 
-import "fmt"
-
 func Barks() string {
 	return "Barks"
 }
 
 func Bark() string {
 	return "Another Barks"
-}
-
-func From11() {
-	fmt.Println("I Am from verion 1.1.0")
-
-}
-
-func From12() {
-	fmt.Println("I Am from verion 1.2.0")
 }
